bea27ba85c4e497a1190afe5124472e544696b48
stelina <stelina.pema@gmail.com>
2023-05-21 21:23:45 +0200

    save

--- Assignments/HospitalSBProject/src/main/java/demo/spdemo06configurationclasses/DemoApplication.java
+++ Assignments/HospitalSBProject/src/main/java/demo/spdemo06configurationclasses/DemoApplication.java
@@ -3,6 +3,9 @@ package demo.spdemo06configurationclasses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.print.Doc;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -22,6 +25,39 @@ public class DemoApplication {
 
         Doctor dr2 = ctx.getBean("getBonus", Doctor.class);
         System.out.println("dr2 " + dr2);
+
+        Doctor specializedDoctor = ctx.getBean("pediatrician", Doctor.class);
+        System.out.println("specialized Doctor: " + specializedDoctor);
+
+
+        Doctor specializedDoctor2 = ctx.getBean("nefrologist", Doctor.class);
+        System.out.println("specialized doctor 2: " + specializedDoctor2);
+
+        HeadOfHospital headOfHospital = ctx.getBean("surgeon", HeadOfHospital.class);
+        System.out.println("The head of hospital is a : " + headOfHospital);
+
+    }
+
+
+
+    @Bean
+    Doctor familyDoctor(){
+        return new Doctor(1000);
+    }
+
+    @Bean(name = {"pediatrician", "nefrologist", "gastrologist"})
+    Doctor specializedDoctor() {
+        return new Doctor(10000);
+    }
+
+    @Bean
+    HeadOfHospital surgeon(){
+        return new HeadOfHospital(specializedDoctor());
+    }
+
+    @Bean
+    HeadOfHospital surgeon2(Doctor doctor){
+        return new HeadOfHospital(doctor);
     }
 
 }
